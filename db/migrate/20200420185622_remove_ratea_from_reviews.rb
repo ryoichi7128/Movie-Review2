f74758ee82b8b5a7e311a5a81d98365f4c2ed1d4
ryoichi7128 <ryoichi7128@gmail.com>
2020-04-28 05:59:48 +0000

    [fix]マイグレーションファイル修正

--- db/migrate/20200420185622_remove_ratea_from_reviews.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRateaFromReviews < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :reviews, :ratea, :float
-  end
-end
--- db/migrate/20200420185658_change_data_rate_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataRateToReviews < ActiveRecord::Migration[5.2]
-  def change
-  	add_column :reviews, :rate, :float
-  end
-end
